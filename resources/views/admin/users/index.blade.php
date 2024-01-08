3af900e7b8cc706e03f901c567b0d488f42d49c3
Adrian Ponce <adrian.ponce@kaicen.com>
2024-01-08 12:01:03 +0100

    events added

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -31,33 +31,35 @@
   </section>
   
   <div class="table-records">
-    @foreach($users as $user_element)
-      <article class="table-record">
-        <div class="table-record-buttons">
-          <div class="edit-button" data-endpoint="{{route('users_edit', ["user" => $user_element->id])}}">
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
-              </svg>
-            </button>
+    @isset($users)
+      @foreach($users as $user_element)
+        <article class="table-record">
+          <div class="table-record-buttons">
+            <div class="edit-button" data-endpoint="{{route('users_edit', ["user" => $user_element->id])}}">
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+                </svg>
+              </button>
+            </div>
+            <div class="destroy-button"  data-endpoint="{{route('users_destroy', ["user" => $user_element->id])}}">
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                </svg>
+              </button>
+            </div>
           </div>
-          <div class="destroy-button"  data-endpoint="{{route('users_destroy', ["user" => $user_element->id])}}">
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-              </svg>
-            </button>
+          <div class="table-data">
+            <ul>
+              <li><span>Email</span>{{$user_element->email}}</li>
+              <li><span>Nombre</span>{{$user_element->name}}</li>
+            </ul>
           </div>
-        </div>
-        <div class="table-data">
-          <ul>
-            <li><span>Email</span>{{$user_element->email}}</li>
-            <li><span>Nombre</span>{{$user_element->name}}</li>
-          </ul>
-        </div>
-      </article>
-    @endforeach
+        </article>
+      @endforeach
+    @endisset
   </div>
 
   <div class="table-pagination">
